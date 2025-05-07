99f56bd8fe6b509de453321ceceb2f4104c1f63c
PrahaldChouhan <prahladchouhan928@gmail.com>
2025-05-08 01:08:34 +0530

    To Do lsit ui Done

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-let AllElem=document.querySelectorAll('.elem');
-let AllFullElem=document.querySelectorAll(".fullelem");
-let AllFullElemBack=document.querySelectorAll(".fullelem .back");
+function elementFeatures() {
+  let AllElem = document.querySelectorAll(".elem");
+  let AllFullElem = document.querySelectorAll(".fullelem");
+  let AllFullElemBack = document.querySelectorAll(".fullelem .back");
 
-let elemCard=document.querySelector(".elemCard");
+  let elemCard = document.querySelector(".elemCard");
 
+  AllElem.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      AllFullElem[elem.id].style.display = "block";
+      elemCard.style.display = "none";
+    });
+  });
 
-AllElem.forEach(function(elem)
-{
-    elem.addEventListener('click',function(){
-        AllFullElem[elem.id].style.display='block';
-        elemCard.style.display="none";
-    })
-});
-
-AllFullElemBack.forEach(function(back)
-{
-    back.addEventListener('click',function(){
-        AllFullElem[back.id].style.display='none';
-        elemCard.style.display="flex";
-    })
-});
-
-
+  AllFullElemBack.forEach(function (back) {
+    back.addEventListener("click", function () {
+      AllFullElem[back.id].style.display = "none";
+      elemCard.style.display = "flex";
+    });
+  });
+}
+elementFeatures();
