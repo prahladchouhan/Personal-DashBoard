c1355a03bac23ad24dc57d1daeb2e5135718c145
PrahaldChouhan <prahladchouhan928@gmail.com>
2025-05-07 18:44:13 +0530

    level ui layout done

---
+++ script.js
@@ -0,0 +1,24 @@
+let AllElem=document.querySelectorAll('.elem');
+let AllFullElem=document.querySelectorAll(".fullelem");
+let AllFullElemBack=document.querySelectorAll(".fullelem .back");
+
+let elemCard=document.querySelector(".elemCard");
+
+
+AllElem.forEach(function(elem)
+{
+    elem.addEventListener('click',function(){
+        AllFullElem[elem.id].style.display='block';
+        elemCard.style.display="none";
+    })
+});
+
+AllFullElemBack.forEach(function(back)
+{
+    back.addEventListener('click',function(){
+        AllFullElem[back.id].style.display='none';
+        elemCard.style.display="flex";
+    })
+});
+
+
